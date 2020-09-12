6aeb06c23d0a1b6d6d969e3bb06e9c34a47bda92
VikrantReddy <vikrantreddy@hotmail.com>
2020-09-12 11:47:39 +0530

    Update

--- app.py
+++ app.py
@@ -16,8 +16,8 @@ def browserLoad(link):
     options.add_argument("--headless")
     options.add_argument("--disable-gpu")
     options.add_argument("--no-sandbox")
-    options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-    drvr = webdriver.Chrome(options = options,executable_path= os.environ.get("CHROMEDRIVER_PATH)"))
+    options.binary_location = GOOGLE_CHROME_BIN
+    drvr = webdriver.Chrome(options = options,executable_path= CHROMEDRIVER_PATH)
     drvr.get(link)
     return drvr
 
